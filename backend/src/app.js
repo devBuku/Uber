f0db9423c909a0fdc391a3376450d1ff3634ac33
devBuku <shubhayanbagchi30@gmail.com>
2026-02-25 09:57:15 +0530

    feat: complete user login flow

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
     res.send("Hello Friend");
--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,3 +1,4 @@
+const User = require("../models/user.model");
 const userService = require("../services/user.service");
 const { validationResult } = require("express-validator");
 
@@ -8,6 +9,10 @@ async function registerUser(req, res) {
     }
     try {
         const { fullname, email, password } = req.body;
+        const isUserAlready = await User.findOne({ email });
+        if (isUserAlready) {
+            return res.status(409).json({ message: `User already exists` });
+        }
         const newUser = await userService.createUser({
             firstname: fullname.firstname,
             lastname: fullname.lastname,
@@ -27,4 +32,36 @@ async function registerUser(req, res) {
     }
 }
 
-module.exports = { registerUser };
+async function loginUser(req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+        const { email, password } = req.body;
+        const isUser = await User.findOne({ email }).select("+password");
+        if (!isUser) {
+            return res
+                .status(401)
+                .json({ message: `Invalid email or password` });
+        }
+        const isValidPassword = isUser.comparePassword(password);
+        if (!isValidPassword) {
+            return res
+                .status(401)
+                .json({ message: `Invalid email or password` });
+        }
+        const token = isUser.generateAuthToken();
+        res.cookie("jwt", token);
+        res.status(200).json({
+            message: `User logged in`,
+            token,
+            isUser,
+        });
+    } catch (error) {
+        console.error(`Error in Login User Controller: ${error}`);
+        res.status(500).json({ message: `Internal Server Error` });
+    }
+}
+
+module.exports = { registerUser, loginUser };
