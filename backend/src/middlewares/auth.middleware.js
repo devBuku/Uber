7fde3950a86b4ed3ed141d6513aa46bd14509b06
devBuku <shubhayanbagchi30@gmail.com>
2026-03-01 20:32:58 +0530

    feat: complete captain authentication flow

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,4 +1,5 @@
 const User = require("../models/user.model");
+const Captain = require("../models/captain.model");
 const BlacklistToken = require("../models/blacklistToken.model");
 const jwt = require("jsonwebtoken");
 async function authUser(req, res, next) {
@@ -6,13 +7,16 @@ async function authUser(req, res, next) {
     if (!token) {
         return res.status(401).json({ message: `Unauthorized` });
     }
-    const isBlacklisted = await BlacklistToken.findOne({ token: token });
-    if (isBlacklisted) {
-        return res.status(401).json({ message: `Unauthorized` });
-    }
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const isBlacklisted = await BlacklistToken.findOne({ token: token });
+        if (isBlacklisted) {
+            return res.status(401).json({ message: `Unauthorized` });
+        }
         const user = await User.findById(decoded.id);
+        if (!user) {
+            return res.status(401).json({ message: `Unauthorized` });
+        }
         req.user = user;
         next();
     } catch (error) {
@@ -20,4 +24,26 @@ async function authUser(req, res, next) {
     }
 }
 
-module.exports = { authUser };
+async function authCaptain(req, res, next) {
+    const token = req.cookies.jwt;
+    if (!token) {
+        return res.status(401).json({ message: `Unauthorized` });
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const isBlacklisted = await BlacklistToken.findOne({ token: token });
+        if (isBlacklisted) {
+            return res.status(401).json({ message: `Unauthorized` });
+        }
+        const captain = await Captain.findById(decoded.id);
+        if (!captain) {
+            return res.status(401).json({ message: `Unauthorized` });
+        }
+        req.captain = captain;
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: `Unauthorized` });
+    }
+}
+
+module.exports = { authUser, authCaptain };
