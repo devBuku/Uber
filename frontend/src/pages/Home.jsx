604829a8f41e66d5198cd0a30cef9c168925d2fe
devBuku <shubhayanbagchi30@gmail.com>
2026-06-10 20:35:17 +0530

    ui: start user home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,38 @@
 const Home = () => {
-    return <div>Home</div>;
+  return (
+    <div className="h-screen relative">
+      <img
+        className="w-16 absolute left-5 top-5"
+        src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"
+        alt=""
+      />
+      <div className="h-screen w-screen">
+        <img
+          alt=""
+          src={null}
+          className="h-full w-full object-cover bg-red-400"
+        />
+      </div>
+      <div className="flex flex-col justify-end absolute h-screen top-0 w-full ">
+        <div className="h-[30%] p-5 bg-white">
+          <h4 className="text-3xl font-semibold">Find a trip</h4>
+          <form>
+            <input
+              className="bg-[#eee] px-12 py-2 text-base rounded-lg w-full mt-5"
+              type="text"
+              placeholder="Add a pick-up location"
+            />
+            <input
+              className="bg-[#eee] px-12 py-2 text-base rounded-lg w-full mt-5"
+              type="text"
+              placeholder="Enter your destination"
+            />
+          </form>
+        </div>
+        <div className="h-[70%] bg-red-500 p-5"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
